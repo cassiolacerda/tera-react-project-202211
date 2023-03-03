881ef21508ded80da6abe8e76cc68a299de7890a
Cássio Lacerda <697508+cassiolacerda@users.noreply.github.com>
2023-03-02 21:26:15 -0300

    Adiciona página de usuários

---
+++ src/components/molecules/UserListWrapper.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import UserListItem from "./UserListItem";
+
+export default function UserListWrapper(props) {
+  return (
+    <div className="users__list">
+      {props.users
+        .sort((a, b) => a.fn.localeCompare(b.fn))
+        .map((user) => (
+          <UserListItem
+            key={user.id}
+            id={user.id}
+            avatar={user.avatar}
+            fn={user.fn}
+            ln={user.ln}
+          />
+        ))}
+    </div>
+  );
+}
